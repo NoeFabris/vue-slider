0b9a89755654fe024aa1b93ef218dc92b76f5793
NoeFabris <noe@elysresidence.com>
2021-05-03 00:33:18 +0200

    Pt.1 (mancano key tastiera)

---
+++ script.js
@@ -0,0 +1,47 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            img: ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg'],
+            indice: 0,
+        },
+        methods: {
+            slidesx: function() {
+                this.indice --
+                if (this.indice < 0) {
+                    this.indice = 3
+                }
+            },
+            slidedx: function() {
+                this.indice ++
+                if (this.indice > 3) {
+                    this.indice = 0
+                }
+            },
+            dot0 : function() {
+                this.indice = 0
+            },
+            dot1 : function() {
+                this.indice = 1
+            },
+            dot2 : function() {
+                this.indice = 2
+            },
+            dot3 : function() {
+                this.indice = 3
+            },
+            keysx: function(event) {
+                this.indice --
+                if (this.indice < 0) {
+                    this.indice = 3
+                }
+            },
+            keydx: function(event) {
+                this.indice ++
+                if (this.indice > 3) {
+                    this.indice = 0
+                }
+            }
+        }
+    }
+)
